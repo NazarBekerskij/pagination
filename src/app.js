98173b2c5504ed1d27ac478709d882f1a85f8aa5
Nazar Bekerskij <bekerskijnazar9@gmail.com>
2026-04-25 10:37:24 +0300

    save render

--- src/app.js
+++ src/app.js
@@ -1,103 +1,59 @@
-// dfe64df20e5048dc81abc33421bdfb23
-
-
-
-const listRef = document.querySelector(".list")
+const listRef = document.querySelector(".list");
 
 let pageSize = 18;
 let page = 1;
-
 let totalPage = 0;
 
-// function getNews(){
-//     return fetch(`https://newsapi.org/v2/everything?q=tesla&from=2026-03-19&sortBy=publishedAt&apiKey=dfe64df20e5048dc81abc33421bdfb23&pageSize=${pageSize}&page=${page}`).then((res) => res.json())
-// }
-
-
 function getNews() {
-    return fetch(`https://newsapi.org/v2/everything?q=tesla&sortBy=publishedAt&apiKey=dfe64df20e5048dc81abc33421bdfb23&pageSize=${pageSize}&page=${page}`)
+  return fetch(`https://gnews.io/api/v4/search?q=tesla&lang=en&max=${pageSize}&page=${page}&apikey=9a420d5dc04e57775b18d56d070814d6`)
     .then((res) => res.json());
 }
 
+
+
+
 getNews().then((res) => {
-    createArticles(res.articles);
-    totalPage = Math.ceil(res.totalResults / pageSize)
-})
-
-
-
-
-
+  createArticles(res.articles);
+  totalPage = Math.ceil(res.totalArticles / pageSize);
+});
 
 function createArticles(array) {
-    const item = array.map(({author, content, description, title, publishedAt, urlToImage}) => {
-        const imageUrl = urlToImage ? urlToImage : "https://hostiq.ua/wiki/wp-content/uploads/2021/05/03-error-404-not-found.png";
-        return `<li class="item">
+  if (!array) return;
+
+  const item = array.map(({ title, description, publishedAt, image, source }) => {
+    const imageUrl = image
+      ? image
+      : "https://hostiq.ua/wiki/wp-content/uploads/2021/05/03-error-404-not-found.png";
+
+    return `<li class="item">
         <h2>${title}</h2>
-        <h2>${author}</h2>
+        <h2>${source?.name || "Unknown"}</h2>
         <p>${publishedAt}</p>
-        <img src="${imageUrl}" alt="${content}">
-    </li>`
-    }).join("")
+        <img src="${imageUrl}" alt="${description}">
+    </li>`;
+  }).join("");
 
-    listRef.insertAdjacentHTML("beforeend", item)
-
+  listRef.insertAdjacentHTML("beforeend", item);
 }
 
+const newsObserver = new IntersectionObserver((items) => {
+  items.forEach((itm) => {
+    if (itm.isIntersecting) {
 
-const newsObserver = new IntersectionObserver((item, observer) => {
-  item.forEach((itm) => {
-    if(itm.isIntersecting){
-       if (totalPage !== 0 && page >= totalPage) {
+      if (totalPage !== 0 && page >= totalPage) {
         newsObserver.unobserve(document.querySelector(".box"));
         return;
       }
-      page += 1
+
+      page += 1;
+
       getNews().then((res) => {
-    createArticles(res.articles);
-   })
-    }    
-  })
-  
-    
+        createArticles(res.articles);
+      });
+    }
+  });
 }, {
-    rootMargin: "250px"
-}) 
+  rootMargin: "250px"
+});
 
-newsObserver.observe(document.querySelector(".box"))
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-// function createNewsItems(array){
-//     const item = array.map(({author, title, description, content, publishedAt, urlToImage}) => {
-//         console.log(urlToImage);
-        
-//         return ` <li class="item">
-//   <h2>${title}</h2>
-//   <h2>${author}</h2>
-//   <p>${content}</p>
-//   <p>${description}</p>
-//   <p>${publishedAt}</p>
-//   <img src="${urlToImage}" alt="${title}">
-// </li>`
-//     }).join("")
-//     listRef.insertAdjacentHTML("beforeend", item);
-// }
+newsObserver.observe(document.querySelector(".box"));
