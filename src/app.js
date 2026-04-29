7cc6ca17b84d0d240952aee00ed21817cbf05782
Nazar Bekerskij <bekerskijnazar9@gmail.com>
2026-04-29 17:08:48 +0300

    save new

--- src/app.js
+++ src/app.js
@@ -4,32 +4,30 @@ let pageSize = 18;
 let page = 1;
 let totalPage = 0;
 
+const API_KEY = "55396511-9fa47eb753a2484966d5aafba";
+
 function getNews() {
-  return fetch(`https://gnews.io/api/v4/search?q=tesla&lang=en&max=${pageSize}&page=${page}&apikey=9a420d5dc04e57775b18d56d070814d6`)
+  return fetch(`https://pixabay.com/api/?key=${API_KEY}&editors_choice=true&per_page=${pageSize}&page=${page}`)
     .then((res) => res.json());
 }
 
-
-
-
 getNews().then((res) => {
-  createArticles(res.articles);
-  totalPage = Math.ceil(res.totalArticles / pageSize);
+  createArticles(res.hits); 
+  totalPage = Math.ceil(res.totalHits / pageSize); 
 });
 
 function createArticles(array) {
   if (!array) return;
 
-  const item = array.map(({ title, description, publishedAt, image, source }) => {
-    const imageUrl = image
-      ? image
+  const item = array.map(({ tags, webformatURL, user }) => {
+    const imageUrl = webformatURL
+      ? webformatURL
       : "https://hostiq.ua/wiki/wp-content/uploads/2021/05/03-error-404-not-found.png";
 
     return `<li class="item">
-        <h2>${title}</h2>
-        <h2>${source?.name || "Unknown"}</h2>
-        <p>${publishedAt}</p>
-        <img src="${imageUrl}" alt="${description}">
+        <h2>${tags}</h2>
+        <h2>${user || "Unknown"}</h2>
+        <img src="${imageUrl}" alt="${tags}">
     </li>`;
   }).join("");
 
@@ -48,7 +46,7 @@ const newsObserver = new IntersectionObserver((items) => {
       page += 1;
 
       getNews().then((res) => {
-        createArticles(res.articles);
+        createArticles(res.hits);
       });
     }
   });
